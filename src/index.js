f020345d73a55a435b5dacc09c7b55662a4546e0
leonardoteixeira018-gif <leonardoteixeira018@gmail.com>
2026-03-11 17:06:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use('/auth',     require('./routes/auth'));
-app.use('/markets',  require('./routes/markets'));
-app.use('/bets',     require('./routes/bets'));
-app.use('/ranking',  require('./routes/ranking'));
-app.use('/deposits', require('./routes/deposits'));
-app.use('/admin',    require('./routes/admin'));
+app.use('/auth',        require('./routes/auth'));
+app.use('/markets',     require('./routes/markets'));
+app.use('/bets',        require('./routes/bets'));
+app.use('/ranking',     require('./routes/ranking'));
+app.use('/deposits',    require('./routes/deposits'));
+app.use('/withdrawals', require('./routes/withdrawals'));
+app.use('/admin',       require('./routes/admin'));
 
 app.get('/health', (req, res) => res.json({ status: 'ok', timestamp: new Date() }));
 app.use((req, res) => res.status(404).json({ error: 'Rota não encontrada' }));
