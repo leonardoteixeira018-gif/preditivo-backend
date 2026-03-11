b79d5f9ba6ff45b99cedcace49b9b692abcd098d
leonardoteixeira018-gif <leonardoteixeira018@gmail.com>
2026-03-11 01:04:29 -0300

    Add routes for deposits and admin

--- src/index.js
+++ src/index.js
@@ -28,3 +28,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+
+app.use('/deposits', require('./routes/deposits'));
+app.use('/admin', require('./routes/admin'));
