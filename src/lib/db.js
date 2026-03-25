5a51679709b55a60b08066ce5d35ae32cf59544f
leonardoteixeira018-gif <leonardoteixeira018@gmail.com>
2026-03-25 11:16:17 -0300

    fix: ssl rejectUnauthorized false for Supabase self-signed cert

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? true
-    : { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000
